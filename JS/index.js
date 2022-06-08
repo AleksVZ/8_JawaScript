e28466c9b9b4a217ecf3f713c9f7ece017031dbe
AleksVZ <zaiko1984@ukr.net>
2022-06-08 16:05:33 +0300

    intial commit

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+// // ЦИКЛЫ
+// //    FOR
+// // for(let i=0; i<10; i++ ){
+// //     saySomething();
+// // }
+// // function saySomething(){
+// //     document.writeln("hello");
+// // }
+// // // Прерывание функции BREAK
+
+// // for(let i= 0; i<100; i ++){
+// //     document.writeln(i);
+// //     if(i == 25){
+// //         break;
+// //     }
+// // }
+
+// // // Пропуск итерации
+// // let floors = 30;
+// // for(i = 1; i <= floors; i ++){
+// //     if(i == 13){
+// //         continue;
+// //     }
+// //     document.writeln("At floor:" +i +"<br>");
+// // }
+
+// // let maxSpeed = 50;
+// // let minSpeed = 40;
+// let number = 45;
+// for(let i=0; i<number; i++){
+//     if(i ==15){
+//         continue;
+//     }
+//     document.writeln(i);
+// }
